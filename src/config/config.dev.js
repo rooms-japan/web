0fa67ea2f85fe5ebfc2b12b6fa1cc3b718a6532e
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2018-01-22 20:46:11 +0100

    Add config

---
+++ src/config/config.dev.js
@@ -0,0 +1,8 @@
+const config = {
+  url: {
+    domain: `http://localhost`,
+    api: `http://localhost:5000/api`
+  }
+};
+
+export default config;
---
+++ src/config/config.prod.js
@@ -0,0 +1,8 @@
+const config = {
+  url: {
+    domain: `http://tiphaineviard.com`,
+    api: `http://tiphaineviard.com:5000/api`
+  }
+};
+
+export default config;
