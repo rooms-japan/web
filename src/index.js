31aa9ff3e6ec6c429207e9512c53e7de3250b6a1
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2018-01-22 19:25:13 +0100

    Move Service Worker code into utils folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import registerServiceWorker from './utils/register-service-worker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// We may want to remove service workers for now as it may hinder our
+// productivity while developping the UI!
+//
+// registerServiceWorker();
---
+++ src/utils/register-service-worker/index.js
@@ -0,0 +1,1 @@
+export { default, unregister } from './registerServiceWorker'
