b5089a5d9216d252059a947ada0f4236b079b0a5
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2018-01-22 20:47:09 +0100

    Implement data layer abstraction

---
+++ src/store/api/api.js
@@ -0,0 +1,39 @@
+import config from '../../config'
+import checkStatus from '../../utils/check-fetch-status'
+
+const api = {
+  init: () => {},
+  get: async (key, params) => {
+    let value, error;
+
+    let resource
+    switch (key) {
+      case 'columns':
+      case 'wards':
+        resource = `info/${key}`
+        break;
+      
+      case 'data':
+        resource = `hello/${params.xcol}/${params.ycol}/${params.ward}`
+        break;
+    
+      default:
+        resource = key
+        break;
+    }
+
+    try {
+      value = await fetch(`${config.url.api}/${resource}`, {
+        method: 'GET',
+      })
+        .then(checkStatus)
+        .then(res => res.json());
+    } catch (err) {
+      error = err;
+    }
+
+    return [value, error]
+  }
+}
+
+export default api
---
+++ src/store/store.js
@@ -0,0 +1,19 @@
+import api from './api'
+
+const store = {
+  init: () => {
+    api.init()
+  },
+
+  get: async (key, params) => {
+    let value, error;
+
+    [value, error] = await api.get(key, params);
+
+    return new Promise((resolve, reject) => {
+      value != null ? resolve(value) : reject(error); 
+    });
+  }
+}
+
+export default store;
