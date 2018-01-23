06b7e8506cd3fae6569e15a45052ca752e72e09a
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2018-01-23 19:25:56 +0100

    Add Select component

---
+++ src/components/Select/Select.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './Select.css';
+
+const Select = ({ label, elements, selected, onSelect, defaultSelected, emptyOption }) => (
+  <label className="select-wrapper">
+    <span className="select-label">{label}</span>
+    {elements && elements.length ? (
+      <select
+        className="select"
+        onChange={e => onSelect(e.target.value)}
+        value={selected}
+      >
+        {emptyOption && <option className="select-option" value="" />}
+        {elements.map(el => (
+          <option className="select-option" key={el.id} value={el.label} selected={defaultSelected === el.id}>
+            {el.label}
+          </option>
+        ))}
+      </select>
+    ) : (
+      <span className="select-empty">No elements to select.</span>
+    )}
+  </label>
+);
+
+export default Select;
