2bf4840ac9aab68c1e98747e59a2e6cd7b72b987
Tiphaine Viard <tiphaine.viard@gmail.com>
2018-06-09 20:52:53 +0900

    Tests with Dash for Python

---
+++ dash/app.py
@@ -0,0 +1,96 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+import urllib.request
+import json
+
+app = dash.Dash()
+
+wards = json.loads(urllib.request.urlopen("http://tiphaineviard.com:5000/api/info/wards").read())
+wards_list = [w['value'] for w in wards]
+print(wards_list)
+
+columns = json.loads(urllib.request.urlopen("http://tiphaineviard.com:5000/api/info/columns").read())
+
+# Download all data :D ?
+df = pd.read_csv('bdd.csv')
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='selectedWards',
+        options=wards,
+        value='shinagawa',
+        multi=True
+    ),
+    html.Div([
+        dcc.Dropdown(
+            id='x',
+            options=columns,
+            value='layout',
+        ),
+        dcc.Dropdown(
+            id='y',
+            options=columns,
+            value='rent',
+        ),
+    ]),
+    dcc.Graph(
+        id='main-graph',
+            )
+])
+
+@app.callback(
+    dash.dependencies.Output('main-graph', 'figure'),
+    [
+        dash.dependencies.Input('selectedWards', 'value'),
+        dash.dependencies.Input('x', 'value'),
+        dash.dependencies.Input('y', 'value')
+    ]
+)
+
+def update_info(w, x, y):
+    # data = urllib.request.urlopen("").read()
+    if type(w) == str:
+        w = [w]
+    print("You want " + x + " against " + y + " for " + str(w) + ".")
+
+
+    data = [
+        go.Box(
+        y=df[(df.layout == i) & (df.location.str.contains('shinagawa', case=False))][y],
+        boxpoints='all',
+        jitter=0.3,
+        pointpos=-1.8,
+        name=i
+    ) for i in df['layout'].unique() if len(df[(df.layout == i) & (df.location.str.contains('shinagawa', case=False))][y]) > 10]
+    
+    # data = [
+        # go.Scatter(
+            # x=df[df.location.str.contains(i, case=False)][x],
+            # y=df[df.location.str.contains(i, case=False)][y],
+            # mode='markers',
+            # opacity=0.7,
+            # name=i,
+            # marker={
+                # 'size': 15,
+                # 'line': {'width':0.5, 'color': 'white'}
+            # }
+        # ) for i in w
+    # ]
+
+    return {
+            'data': data,
+            'layout': go.Layout(
+                    xaxis={'title': x},
+                    yaxis={'title': y},
+                    margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest'
+                )
+        }
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
